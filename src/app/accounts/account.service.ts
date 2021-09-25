2fa6d2e26fc0eabf100866df7072c75cb4f39ef5
jacob-kubica <goldsugarwithice@gmail.com>
2021-09-25 11:42:11 -0400

    preeupdate

--- src/app/accounts/account.service.ts
+++ src/app/accounts/account.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Account } from "./account.model";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
 
-import { Account } from './account.model';
-
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AccountsService {
   private accounts: Account[] = [];
   private accountsUpdated = new Subject<Account[]>();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   getAccounts() {
     this.http
-      .get<{ message: string; accounts: any }>('http://localhost:3000/api/accounts')
+      .get<{ message: string; accounts: any }>(
+        "http://localhost:3000/api/accounts"
+      )
       .pipe(
-        map(accountData => {
-          return accountData.accounts.map(account => {
+        map((accountData) => {
+          return accountData.accounts.map((account) => {
             return {
               title: account.title,
               content: account.content,
@@ -30,12 +31,12 @@ export class AccountsService {
               dueDate: account.dueDate,
               institution: account.institution,
               balance: account.balance,
-              imagePath: account.imagePath
+              imagePath: account.imagePath,
             };
           });
         })
       )
-      .subscribe(transformedAccounts => {
+      .subscribe((transformedAccounts) => {
         this.accounts = transformedAccounts;
         this.accountsUpdated.next([...this.accounts]);
       });
@@ -47,20 +48,18 @@ export class AccountsService {
 
   getAccount(id: string) {
     return this.http.get<{
-      _id: string,
-      title: string,
-      content: string,
-      name: string,
-      description: string,
-      interest: number,
-      limit: number,
-      dueDate: Date,
-      institution: string,
-      balance: number,
-      imagePath: string
-    }>(
-      'http://localhost:3000/api/accounts/' + id
-    );
+      _id: string;
+      title: string;
+      content: string;
+      name: string;
+      description: string;
+      interest: number;
+      limit: number;
+      dueDate: Date;
+      institution: string;
+      balance: number;
+      imagePath: string;
+    }>("http://localhost:3000/api/accounts/" + id);
   }
 
   addAccount(
@@ -76,22 +75,22 @@ export class AccountsService {
     image: File
   ) {
     const accountData = new FormData();
-    accountData.append('title', title);
-    accountData.append('content', content);
-    accountData.append('name', name);
-    accountData.append('description', description);
-    accountData.append('interest', interest);
-    accountData.append('limit', limit);
-    accountData.append('dueDate', dueDate);
-    accountData.append('institution', institution);
-    accountData.append('balance', balance);
-    accountData.append('image', image, title);
+    accountData.append("title", title);
+    accountData.append("content", content);
+    accountData.append("name", name);
+    accountData.append("description", description);
+    accountData.append("interest", interest);
+    accountData.append("limit", limit);
+    accountData.append("dueDate", dueDate);
+    accountData.append("institution", institution);
+    accountData.append("balance", balance);
+    accountData.append("image", image, title);
     this.http
       .post<{ message: string; account: Account }>(
-        'http://localhost:3000/api/accounts',
+        "http://localhost:3000/api/accounts",
         accountData
       )
-      .subscribe(responseData => {
+      .subscribe((responseData) => {
         const account: Account = {
           id: responseData.account.id,
           title: title,
@@ -103,11 +102,11 @@ export class AccountsService {
           dueDate: dueDate,
           institution: institution,
           balance: balance,
-          imagePath: responseData.account.imagePath
+          imagePath: responseData.account.imagePath,
         };
         this.accounts.push(account);
         this.accountsUpdated.next([...this.accounts]);
-        this.router.navigate(['/']);
+        this.router.navigate(["/"]);
       });
   }
 
@@ -126,19 +125,19 @@ export class AccountsService {
   ) {
     let accountData: Account | FormData;
 
-    if (typeof image === 'object') {
+    if (typeof image === "object") {
       accountData = new FormData();
-      accountData.append('id', id);
-      accountData.append('title', title);
-      accountData.append('content', content);
-      accountData.append('name', name);
-      accountData.append('description', description);
-      accountData.append('interest', interest);
-      accountData.append('limit', limit);
-      accountData.append('dueDate', dueDate);
-      accountData.append('institution', institution);
-      accountData.append('balance', balance);
-      accountData.append('image', image, title);
+      accountData.append("id", id);
+      accountData.append("title", title);
+      accountData.append("content", content);
+      accountData.append("name", name);
+      accountData.append("description", description);
+      accountData.append("interest", interest);
+      accountData.append("limit", limit);
+      accountData.append("dueDate", dueDate);
+      accountData.append("institution", institution);
+      accountData.append("balance", balance);
+      accountData.append("image", image, title);
     } else {
       accountData = {
         id: id,
@@ -151,14 +150,14 @@ export class AccountsService {
         dueDate: dueDate,
         institution: institution,
         balance: balance,
-        imagePath: image
+        imagePath: image,
       };
     }
     this.http
-      .put('http://localhost:3000/api/accounts/' + id, accountData)
-      .subscribe(response => {
+      .put("http://localhost:3000/api/accounts/" + id, accountData)
+      .subscribe((response) => {
         const updatedAccounts = [...this.accounts];
-        const oldAccountIndex = updatedAccounts.findIndex(p => p.id === id);
+        const oldAccountIndex = updatedAccounts.findIndex((p) => p.id === id);
         const account: Account = {
           id: id,
           title: title,
@@ -170,20 +169,22 @@ export class AccountsService {
           dueDate: dueDate,
           institution: institution,
           balance: balance,
-          imagePath: ''
+          imagePath: "",
         };
         updatedAccounts[oldAccountIndex] = account;
         this.accounts = updatedAccounts;
         this.accountsUpdated.next([...this.accounts]);
-        this.router.navigate(['/']);
+        this.router.navigate(["/"]);
       });
   }
 
   deleteAccount(accountId: string) {
     this.http
-      .delete('http://localhost:3000/api/accounts/' + accountId)
+      .delete("http://localhost:3000/api/accounts/" + accountId)
       .subscribe(() => {
-        const updatedAccounts = this.accounts.filter(account => account.id !== accountId);
+        const updatedAccounts = this.accounts.filter(
+          (account) => account.id !== accountId
+        );
         this.accounts = updatedAccounts;
         this.accountsUpdated.next([...this.accounts]);
       });
